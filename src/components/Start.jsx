09908fa4f798b88d406d01b1ab9b175922e8e287
RahulGaur998 <gaur.rahul996@gmail.com>
2022-12-17 14:46:48 +0530

    ADD: Added logo to App component solved to see logo

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -5,7 +5,7 @@ const Start = ({setShowQuiz, showQuiz, bestScore}) => {
 
   return (
     <div className='start'>
-        <img src='./../public/icons8-q-100.png' style={{width: '150px',marginBottom: '20px'}} />
+        <img src='../../icons8-q-100.png' style={{width: '150px',marginBottom: '20px'}} />
         <div className='quiz-head'>Quizzical</div>
         <div className='description'>Check your knowledge?</div>
         <button className='start-quiz' onClick={()=>{setShowQuiz(!showQuiz)}} >Start quiz</button>
